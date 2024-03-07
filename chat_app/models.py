1ec14987037c65b3c31818c07058e9a690192eda
Imad Selka <imadselkawi133@gmail.com>
2024-03-07 22:57:42 +0100

    Add blank=True to text_input field in ChatBot model

--- chat_app/models.py
+++ chat_app/models.py
@@ -8,7 +8,7 @@ class ChatBot(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="GeminiUser", null=True
     )
-    text_input = models.CharField(max_length=500)
+    text_input = models.CharField(max_length=500, blank=True)
     gemini_output = models.TextField(null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
 
